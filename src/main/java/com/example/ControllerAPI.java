5d6960655d13944ec3ea5beda505d7ff6662e0d2
harshit-kumar78 <harshit3551@gmail.com>
2023-02-27 14:26:02 +0530

    request body done

--- src/main/java/com/example/ControllerAPI.java
+++ src/main/java/com/example/ControllerAPI.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,31 +18,44 @@ import org.springframework.web.bind.annotation.RestController;
 
 class Customer {
 	private String name ;
-	public int age;
-	public Customer(String name,int age) {
+	private int age;
+	private String hello;
+	public Customer(String name,int age,String hello) {
 		this.name = name;
 		this.age = age;
+		this.hello = hello;
 	}
 	public String getName() {
 		return this.name;
 	}
-	public int getAge() {
-		return this.age;
+//	public int getAge() {
+//		return this.age;
+//	}
+//	
+//	public String getHello() {
+//		return this.hello;
+//	}
+	
+	@Override
+	public String toString() {
+		return "Customer [name=" + name + ", age=" + age + ", hello=" + hello + "]";
 	}
+	
 }
 @RestController
 @RequestMapping(value="/harshit")
 public class ControllerAPI {
 	
-	@GetMapping(value="/get/{pathvariable}/{age}")
-	public ResponseEntity<Customer> getData(@PathVariable() String pathvariable ,@PathVariable("age") Integer years) {
-//		String response = "get data";
-		Customer c = new Customer(pathvariable,years);
-		return new ResponseEntity<>(c,HttpStatus.OK);
-	}
+//	@GetMapping(value="/get/{pathvariable}/{age}")
+//	public ResponseEntity<Customer> getData(@PathVariable() String pathvariable ,@PathVariable("age") Integer years) {
+////		String response = "get data";
+//		Customer c = new Customer(pathvariable,years);
+//		return new ResponseEntity<>(c,HttpStatus.OK);
+//	}
 	@PostMapping(value="/post")
-	public ResponseEntity<Customer> postData(@RequestParam("url") String name,@RequestParam Integer age) {
-		Customer c = new Customer(name,age);
+	public ResponseEntity<Customer> postData(@RequestBody Customer c) {
+//		Customer c = new Customer("harshit",23);
+		System.out.println(c);
 		return new ResponseEntity<>(c,HttpStatus.CREATED);
 	}
 	
